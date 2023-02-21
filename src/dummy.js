73f678caec6fdbe50c2322fb4f92a8d4923c925b
Shiv Baran Singh <spyshiv@gmail.com>
2023-02-22 04:09:49 +0530

    Update dummy js
    - Removed Jquery Dependency

--- src/dummy.js
+++ src/dummy.js
@@ -1,27 +1,13 @@
 //Project Name: Dummy JS
-//Version: 1.0
-//created: 5/11/2015
+//Version: 2.0
+//Last Modified: 22/02/2023
 //Author: Shiv Baran Singh
-//Author URI: http://shivbaran.in/
-var Dummy;
-(function(){
-    //default Values
-     Dummy = function() {
-        //Default values.
-        this.type = null;
-        this.query = null;
-        this.data = null;
-    };     
-    //static variables
-    Dummy.TEXT = 2;
-    Dummy.TYPE = {
-        PARAGRAPH: 1,
-        SENTENCE: 2,
-        WORD: 3
-    };
-    
-    //words for dummytext
-    Dummy.WORDS = [
+//Author Email: spyshiv@gmail.com
+
+// A self-invoking anonymous function to prevent global scope pollution
+(function() {
+  // An array of words to use in generating dummy content
+  const DUMMY_WORDS_ARR = [
         "lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipiscing", "elit", "ut", "aliquam,", "purus", "sit", "amet", "luctus", "venenatis,", "lectus", "magna", "fringilla", "urna,", "porttitor", "rhoncus", "dolor", "purus", "non", "enim", "praesent", "elementum", "facilisis", "leo,", "vel", "fringilla", "est", "ullamcorper", "eget", "nulla", "facilisi", "etiam", "dignissim", "diam", "quis", "enim", "lobortis", "scelerisque", "fermentum", "dui", "faucibus", "in", "ornare", "quam", "viverra", "orci", "sagittis", "eu", "volutpat", "odio", "facilisis", "mauris", "sit", "amet", "massa", "vitae", "tortor", "condimentum", "lacinia", "quis", "vel", "eros", "donec", "ac", "odio", "tempor", "orci", "dapibus", "ultrices", "in", "iaculis", "nunc", "sed", "augue", "lacus,", "viverra", "vitae", "congue", "eu,", "consequat", "ac", "felis", "donec", "et", "odio", "pellentesque", "diam", "volutpat", "commodo", "sed", "egestas", "egestas", "fringilla", "phasellus", "faucibus", "scelerisque", "eleifend", "donec", "pretium", "vulputate", "sapien", "nec", "sagittis", "aliquam", "malesuada", "bibendum", "arcu", "vitae", "elementum",
         "curabitur", "vitae", "nunc", "sed", "velit", "dignissim", "sodales", "ut", "eu", "sem", "integer", "vitae", "justo", "eget", "magna", "fermentum", "iaculis", "eu", "non", "diam", "phasellus", "vestibulum", "lorem", "sed", "risus", "ultricies", "tristique", "nulla", "aliquet", "enim", "tortor,", "at", "auctor", "urna", "nunc", "id", "cursus", "metus", "aliquam", "eleifend", "mi", "in", "nulla", "posuere", "sollicitudin", "aliquam", "ultrices", "sagittis", "orci,", "a", "scelerisque", "purus", "semper", "eget", "duis", "at", "tellus", "at", "urna", "condimentum", "mattis", "pellentesque", "id", "nibh", "tortor,", "id", "aliquet", "lectus", "proin", "nibh", "nisl,", "condimentum", "id", "venenatis", "a,", "condimentum", "vitae", "sapien", "pellentesque", "habitant", "morbi", "tristique", "senectus", "et", "netus", "et", "malesuada", "fames", "ac", "turpis", "egestas", "sed", "tempus,", "urna", "et", "pharetra", "pharetra,", "massa", "massa", "ultricies", "mi,", "quis", "hendrerit", "dolor", "magna", "eget", "est", "lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipiscing", "elit", "pellentesque", "habitant", "morbi", "tristique", "senectus", "et", "netus", "et", "malesuada", "fames", "ac", "turpis", "egestas", "integer", "eget", "aliquet", "nibh", "praesent", "tristique", "magna", "sit", "amet", "purus", "gravida", "quis", "blandit", "turpis", "cursus", "in", "hac", "habitasse", "platea", "dictumst", "quisque", "sagittis,", "purus", "sit", "amet", "volutpat", "consequat,", "mauris", "nunc", "congue", "nisi,", "vitae", "suscipit", "tellus", "mauris", "a", "diam",
         "maecenas", "sed", "enim", "ut", "sem", "viverra", "aliquet", "eget", "sit", "amet", "tellus", "cras", "adipiscing", "enim", "eu", "turpis", "egestas", "pretium", "aenean", "pharetra,", "magna", "ac", "placerat", "vestibulum,", "lectus", "mauris", "ultrices", "eros,", "in", "cursus", "turpis", "massa", "tincidunt", "dui", "ut", "ornare", "lectus", "sit", "amet", "est", "placerat", "in", "egestas", "erat", "imperdiet", "sed", "euismod", "nisi", "porta", "lorem", "mollis", "aliquam", "ut", "porttitor", "leo", "a", "diam", "sollicitudin", "tempor", "id", "eu", "nisl", "nunc", "mi", "ipsum,", "faucibus", "vitae", "aliquet", "nec,", "ullamcorper", "sit", "amet", "risus", "nullam", "eget", "felis", "eget", "nunc", "lobortis", "mattis", "aliquam", "faucibus", "purus", "in", "massa", "tempor", "nec", "feugiat", "nisl", "pretium", "fusce", "id", "velit", "ut", "tortor", "pretium", "viverra", "suspendisse", "potenti", "nullam", "ac", "tortor", "vitae", "purus", "faucibus", "ornare", "suspendisse", "sed", "nisi", "lacus,", "sed", "viverra", "tellus", "in", "hac", "habitasse", "platea", "dictumst", "vestibulum", "rhoncus", "est", "pellentesque", "elit", "ullamcorper", "dignissim", "cras", "tincidunt", "lobortis", "feugiat", "vivamus", "at", "augue", "eget", "arcu", "dictum", "varius", "duis", "at", "consectetur", "lorem",
@@ -32,96 +18,72 @@ var Dummy;
         "sed", "id", "semper", "risus", "in", "hendrerit", "gravida", "rutrum", "quisque", "non", "tellus", "orci,", "ac", "auctor", "augue", "mauris", "augue", "neque,", "gravida", "in", "fermentum", "et,", "sollicitudin", "ac", "orci", "phasellus", "egestas", "tellus", "rutrum", "tellus", "pellentesque", "eu", "tincidunt", "tortor", "aliquam", "nulla", "facilisi", "cras", "fermentum,", "odio", "eu", "feugiat", "pretium,", "nibh", "ipsum", "consequat", "nisl,", "vel", "pretium", "lectus", "quam", "id", "leo", "in", "vitae", "turpis", "massa", "sed", "elementum", "tempus", "egestas", "sed", "sed", "risus", "pretium", "quam", "vulputate", "dignissim", "suspendisse", "in", "est", "ante", "in", "nibh", "mauris,", "cursus", "mattis", "molestie", "a,", "iaculis", "at", "erat",
         "pellentesque", "adipiscing", "commodo", "elit,", "at", "imperdiet", "dui", "accumsan", "sit", "amet", "nulla", "facilisi", "morbi", "tempus", "iaculis", "urna,", "id", "volutpat", "lacus", "laoreet", "non", "curabitur", "gravida", "arcu", "ac", "tortor", "dignissim", "convallis", "aenean", "et", "tortor", "at", "risus", "viverra", "adipiscing", "at", "in", "tellus", "integer", "feugiat", "scelerisque", "varius", "morbi", "enim", "nunc,", "faucibus", "a", "pellentesque", "sit", "amet,", "porttitor", "eget", "dolor", "morbi", "non", "arcu", "risus,", "quis", "varius", "quam", "quisque", "id", "diam", "vel", "quam", "elementum", "pulvinar", "etiam", "non", "quam", "lacus", "suspendisse", "faucibus", "interdum", "posuere", "lorem", "ipsum", "dolor", "sit", "amet,", "consectetur", "adipiscing", "elit", "duis", "tristique", "sollicitudin", "nibh", "sit", "amet", "commodo", "nulla", "facilisi",
         "nullam", "vehicula", "ipsum", "a", "arcu", "cursus", "vitae", "congue", "mauris", "rhoncus", "aenean", "vel", "elit", "scelerisque", "mauris", "pellentesque", "pulvinar", "pellentesque", "habitant", "morbi", "tristique", "senectus", "et", "netus", "et", "malesuada", "fames", "ac", "turpis", "egestas", "maecenas", "pharetra", "convallis", "posuere", "morbi", "leo", "urna,", "molestie", "at", "elementum", "eu,", "facilisis", "sed", "odio", "morbi", "quis", "commodo", "odio", "aenean", "sed", "adipiscing", "diam", "donec", "adipiscing", "tristique", "risus", "nec", "feugiat", "in", "fermentum", "posuere", "urna", "nec", "tincidunt", "praesent", "semper", "feugiat", "nibh", "sed", "pulvinar", "proin", "gravida", "hendrerit", "lectus", "a", "molestie"
-    ];
-    //Random Integer Method 
-    Dummy.prototype.randomInt = function (min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    };
-    //text creator method with parameters: how many, what
-    Dummy.prototype.createText = function(count, type) {
-        switch (type) {
-            //paragraphs are loads of sentences.
-            case Dummy.TYPE.PARAGRAPH:
-                var paragraphs = new Array;
-                for (var i = 0; i < count; i++) {
-                    var paragraphLength = this.randomInt(10, 20);
-                    var paragraph = this.createText(paragraphLength, Dummy.TYPE.SENTENCE);
-                    paragraphs.push('<p>'+paragraph+'</p>');
-                }
-                return paragraphs.join('\n');
-                break;
-            //sentences are loads of words.
-            case Dummy.TYPE.SENTENCE:
-                var sentences = new Array;
-                for (var i = 0; i < count; i++) {
-                    var sentenceLength = this.randomInt(5, 10);
-                    var words = this.createText(sentenceLength, Dummy.TYPE.WORD).split(' ');
-                    words[0] = words[0].substr(0, 1).toUpperCase() + words[0].substr(1);
-                    var sentence = words.join(' ');
+  ];
 
-                    sentences.push(sentence);
-                }
-                return (sentences.join('. ') + '.').replace(/(\.\,|\,\.)/g, '.');
-                break;
-            //words are words
-            case Dummy.TYPE.WORD:
-                var wordIndex = this.randomInt(0, Dummy.WORDS.length - count - 1);
-
-                return Dummy.WORDS.slice(wordIndex, wordIndex + count).join(' ').replace(/\.|\,/g, '');
-                break;
-            }
-        };
-
-       Dummy.prototype.createDummy = function(element) {
-
-        var dummy = new Array;
-        var count;
-        
-        if (/\d+-\d+[psw]/.test(this.query)){
-            var range = this.query.replace(/[a-z]/,'').split("-");
-            count = Math.floor(Math.random() * parseInt(range[1])) + parseInt(range[0]);
-        }else{
-            count = parseInt(this.query); 
-        }
-        
-        if (/\d+p/.test(this.query)) {
-            var type = Dummy.TYPE.PARAGRAPH;
-        }
-        else if (/\d+s/.test(this.query)) {
-            var type = Dummy.TYPE.SENTENCE;
-        }
-        else if (/\d+w/.test(this.query)) {
-            var type = Dummy.TYPE.WORD;
-        }
-
-        dummy.push(this.createText(count, type));
-        dummy = dummy.join(' ');
-
-        if (element) {
-            if (this.type == Dummy.TEXT)
-                element.innerHTML += dummy;
-        }
-
-        if (element == null)
-            return dummy;
-    };
-    //Register as jQuery
-    if (typeof jQuery != 'undefined') {
-        (function($) {
-            $.fn.dummy = function() {
-                $(this).each(function() {
-                    var dummy = new Dummy;
-                    dummy.type = Dummy.TEXT;
-                    //data-Dummy can be taken with data function (thanks to http://forrst.com/people/webking)
-                    dummy.query = $(this).data('dummy');
-                    dummy.createDummy(this);
-                })
-            };
-
-            //If developer run this javascript, then we can run the Dummy.js
-            $(document).ready(function() {
-                $('[data-dummy]').dummy();
-            });
-        })(jQuery);
+  // Function to generate random text of a given length using words from the given array
+  function generateText(length, wordsArr) {
+    let text = '';
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * wordsArr.length);
+      text += `${wordsArr[randomIndex]} `;
     }
+    return text;
+  }
+
+  // Function to generate a given number of paragraphs using the generateText() function and insert them into a given element
+  function generateParagraphs(numParagraphs, wordsArr, element) {
+    let text = '';
+    for (let i = 0; i < numParagraphs; i++) {
+      const randomLength = Math.floor(Math.random() * 50) + 10;
+      text += `<p>${generateText(randomLength, wordsArr)}</p>`;
+    }
+    element.innerHTML = text;
+  }
+
+  // Function to generate a given number of sentences using the generateText() function and insert them into a given element
+  function generateSentences(numSentences, wordsArr, element) {
+    let text = '';
+    for (let i = 0; i < numSentences; i++) {
+      const randomLength = Math.floor(Math.random() * 10) + 5;
+      text += `${generateText(randomLength, wordsArr)}. `;
+    }
+    element.innerHTML = text;
+  }
+
+  // Function to generate a given number of words using the generateText() function and insert them into a given element
+  function generateWords(numWords, wordsArr, element) {
+    const text = generateText(numWords, wordsArr);
+    element.innerHTML = text;
+  }
+
+  // Function to initialize the dummy content generation
+  function init() {
+    // Get all elements with the 'data-dummy' attribute
+    const dummyElements = document.querySelectorAll('[data-dummy]');
+    const len = dummyElements.length;
+    for (let i = 0; i < len; i++) {
+      const element = dummyElements[i];
+      // Extract the number and type of dummy content to generate from the 'data-dummy' attribute value
+      const [num, type] = element.getAttribute('data-dummy').split(/(\d+)/).filter(Boolean);
+
+      // Generate and insert the appropriate type of dummy content into the element based on the extracted type
+      switch (type) {
+        case 'P':
+          generateParagraphs(parseInt(num, 10), DUMMY_WORDS_ARR, element);
+          break;
+        case 'S':
+          generateSentences(parseInt(num, 10), DUMMY_WORDS_ARR, element);
+          break;
+        case 'w':
+          generateWords(parseInt(num, 10), DUMMY_WORDS_ARR, element);
+          break;
+        default:
+          console.error(`Unknown data-dummy type: ${type}`);
+      }
+    }
+  }
+
+  // Call the init() function to start generating dummy content on page load
+  init();
 })();
+
